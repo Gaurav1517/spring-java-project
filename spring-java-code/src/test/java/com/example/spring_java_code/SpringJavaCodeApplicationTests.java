033466c474c5c06447920b5176bf89a17cc69aeb
Gaurav1517 <145908403+Gaurav1517@users.noreply.github.com>
2024-09-09 18:28:04 +0530

    Add files via upload

---
+++ spring-java-code/src/test/java/com/example/spring_java_code/SpringJavaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_java_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
